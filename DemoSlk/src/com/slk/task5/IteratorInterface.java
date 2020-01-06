74c306e196ce1a9d374b88ffa5b759b8b1e3e1a9
Tarun patel <tarunpatel2982@gmail.com>
2020-01-07 01:39:45 +0530

    addclass

--- DemoSlk/src/com/slk/task5/IteratorInterface.java
+++ DemoSlk/src/com/slk/task5/IteratorInterface.java
@@ -1,10 +1,101 @@
 package com.slk.task5;
 
-public class IteratorInterface {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
+public class IteratorInterface {
+	
+	
+	
+	
+	
+	
+	public static void IteratorInterfaceAllMethod()
+	{
+		ArrayList<String> list = new ArrayList<>();
+        
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add("D");
+		 
+		Iterator<String> iterator = list.iterator();
+		 
+		while(iterator.hasNext()) {
+		    System.out.println( iterator.next() );
+		    String value=iterator.next();
+		    if(value.equals("B"))
+		    		{
+		    			iterator.remove();
+		    		}
+		    
+		}
+		System.out.println(list);
+		
+	}
+	
+	public static void  hashSetIterateExample()
+	{
+		System.out.println("hashSetIterateExample() ===>>>");
+		HashSet<String>  hashSet = new HashSet<>();
+		hashSet.add("A");
+		hashSet.add("B");
+		hashSet.add("C");
+		hashSet.add("D");
+		System.out.println(hashSet);
+		
+		Iterator<String> iterator= hashSet.iterator();
+		while (iterator.hasNext())
+		{
+			String st =  iterator.next();
+			System.out.println(st);
+			
+			if(st.equals("B"))
+			{
+				iterator.remove();
+			}
+			
+		}
+		
+		System.out.println(hashSet);
+	}
+	public static void hashMapKeysIteratorExample()
+	{
+		System.out.println("====>>>> hashMapKeysIteratorExample");
+		HashMap<Integer, String> map= new HashMap<>();
+		map.put(1, "A");
+		map.put(2, "B");
+		map.put(3, "C");
+		map.put(4, "D");
+		System.out.println(map);
+		Iterator<String> iterator = map.values().iterator();
+		
+		while (iterator.hasNext()) {
+			//String string = (String) iterator.next();
+			System.out.println(iterator.next());
+		}
+
+//		Iterator<String> iterator= map.containsKey(key).iterator();
+	}
+	public static void forEachRemaniningExample()
+	{
+		System.out.println("===>>> forEachRemaniningExample");
+		ArrayList<String> arrayList = new ArrayList<>();
+		arrayList.add("tar");
+		arrayList.add("b");
+		arrayList.add("c");
+		arrayList.add("d");
+		
+		arrayList.iterator().forEachRemaining(System.out::println);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		IteratorInterface.IteratorInterfaceAllMethod();
+		IteratorInterface.hashSetIterateExample();
+		IteratorInterface.hashMapKeysIteratorExample();
+		IteratorInterface.forEachRemaniningExample();
 	}
 
 }
