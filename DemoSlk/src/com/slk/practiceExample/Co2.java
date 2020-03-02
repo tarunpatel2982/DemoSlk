daba75006adf3efd0efa3d95b782aa45cad447f5
Tarun patel <tarunpatel2982@gmail.com>
2020-03-02 23:58:35 +0530

    03-02-2020

---
+++ DemoSlk/src/com/slk/practiceExample/Co2.java
@@ -0,0 +1,5 @@
+package com.slk.practiceExample;
+
+public class Co2 {
+
+}
---
+++ DemoSlk/src/com/slk/practiceExample/Ti.java
@@ -0,0 +1,10 @@
+package com.slk.practiceExample;
+
+public class Ti extends Thread{
+
+	public synchronized void  add()
+	{
+		
+		
+	}
+}
